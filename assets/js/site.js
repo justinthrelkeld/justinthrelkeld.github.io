23585686852a3add9916c73ea48d8a29ca3f9d5b
Justin Threlkeld <justin.threlkeld@gmail.com>
2014-03-13 20:10:41 -0500

    first commit under the new system

---
+++ assets/js/site.js
@@ -0,0 +1,11 @@
+$(".projects-showcase").bxSlider({
+  slideSelector: 'article',
+  pagerSelector:'.projects-showcase--nav',
+  adaptiveHeight: true,
+  pager: false,
+  slideWidth: 0,
+  nextText: "<a href='#next' class='nav--next' title='next'>next</a>",
+  prevText: "<a href='#prev' class='nav--prev' title='previous'>previous</a>",
+  nextSelector: '.projects-showcase--nav',
+  prevSelector: '.projects-showcase--nav'
+});
