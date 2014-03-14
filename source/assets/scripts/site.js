e48e1c2541baae7274fc7a8c3b169390898baad4
Justin Threlkeld <justin.threlkeld@gmail.com>
2014-03-13 19:45:10 -0500

    finish the featured projects section

--- source/assets/scripts/site.js
+++ source/assets/scripts/site.js
@@ -1,6 +1,11 @@
 $(".projects-showcase").bxSlider({
+  slideSelector: 'article',
+  pagerSelector:'.projects-showcase--nav',
   adaptiveHeight: true,
   pager: false,
-  nextText: "<abbr title='next' class='fa fa-angle-right'> > </abbr>",
-  prevText: "<abbr title='previous' class='fa fa-angle-left'> < </abbr>"
+  slideWidth: 0,
+  nextText: "<a href='#next' class='nav--next' title='next'>next</a>",
+  prevText: "<a href='#prev' class='nav--prev' title='previous'>previous</a>",
+  nextSelector: '.projects-showcase--nav',
+  prevSelector: '.projects-showcase--nav'
 });
